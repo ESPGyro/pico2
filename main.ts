bb3245511e2f69a309e0e3f7aecc22530711131d
ESP Gyro <99704174+ESPGyro@users.noreply.github.com>
2024-01-29 18:45:15 +0800

    Update main.ts

--- main.ts
+++ main.ts
@@ -5,10 +5,9 @@ namespace Picogame {
   function readmsg(): number {
     let i2cbuf = pins.createBuffer(1);
     i2cbuf[0] = send_id;
-  try {
+    try {
         pins.i2cWriteBuffer(PG_ADDR, i2cbuf);
         let readbuf = pins.i2cReadBuffer(PG_ADDR, 1);
-
         if (readbuf !== undefined) {
           let receivedData = readbuf[0];
             return receivedData;
@@ -16,8 +15,7 @@ namespace Picogame {
             return -1;
         }
     } catch (error) {
-        // 處理錯誤或返回特定的錯誤值
-        return -1; // 或者選擇返回其他適當的值來表示錯誤狀況
+        return -1;
     }
 }
 	
@@ -25,19 +23,18 @@ namespace Picogame {
     export function i2cRead(): number {
 	let i2cbuf = pins.createBuffer(1);
           i2cbuf[0] = send_id; 
-try {
-        pins.i2cWriteBuffer(PG_ADDR, i2cbuf);
-        let readbuf = pins.i2cReadBuffer(PG_ADDR, 1);
-
-        if (readbuf !== undefined) {
-         let receivedData = readbuf[0];
+        try {
+          pins.i2cWriteBuffer(PG_ADDR, i2cbuf);
+          let readbuf = pins.i2cReadBuffer(PG_ADDR, 1);
+          if (readbuf !== undefined) {
+            let receivedData = readbuf[0];
             return receivedData;
-        } else {
-            return -1;
-        }
-    } catch (error) {
-        return -1; // 或者選擇返回其他適當的值來表示錯誤狀況
-    }
+          } else {
+             return -1;
+          }
+       } catch (error) {
+           return -1; // 或者選擇返回其他適當的值來表示錯誤狀況
+       }
     }
 
     //% blockId="sensor_write" block="Broadcast value |%v"
@@ -53,7 +50,6 @@ try {
      game.onUpdate(function () {
         try {
             let revData = readmsg();
-
             // Assuming -1 is returned by readmsg in case of an error
             if (revData !== -1 && revData !== 0 && revData !== 231) {
                 handler(revData);
@@ -62,5 +58,5 @@ try {
     
         }
     });
- }	
+  }	
 }
